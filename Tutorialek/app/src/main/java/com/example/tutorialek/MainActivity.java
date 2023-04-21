f78a87f490b4c27516bef1998957da073624c29e
Kubka <kenoicz@gmail.com>
2023-04-21 13:01:25 +0200

    fotoaparat bongecko

--- Tutorialek/app/src/main/java/com/example/tutorialek/MainActivity.java
+++ Tutorialek/app/src/main/java/com/example/tutorialek/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.tutorialek;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import com.example.tutorialek.databinding.ContentMainBinding;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,20 +30,66 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private CameraManager cameraManager;
+    private String cameraId;
+    private CameraDevice cameraDevice;
+    private static final int CAMERA_PERMISSION_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.content_main);
 
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String[] cameraIds = cameraManager.getCameraIdList();
+            for (String id : cameraIds) {
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
+                Integer lensFacing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (lensFacing != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    cameraId = id;
+                    break;
+                }
+            }
+        } catch (CameraAccessException e) {
+            Log.e("", "Camera initialization failed", e);
+        }
     }
 
-    protected void next(Button b){
-        setContentView(R.layout.snimek);
-        //Log.d("uspech", "je to vpoho");
+    public void next(View v){
+        openCamera();
+
     }
 
+    private void openCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
+            return;
+        }
 
+        try {
+            cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    cameraDevice = camera;
+                    // Camera is now ready to use
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+                    camera.close();
+                    cameraDevice = null;
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {
+                    camera.close();
+                    cameraDevice = null;
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            Log.e("","Camera initialization failed", e);
+        }
+    }
 }
